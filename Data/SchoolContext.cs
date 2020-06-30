8a7672baab41dc48a7ee6c18ec88ae04a94b5089
Jehlum <fshaheen.cs@gmail.com>
2020-06-30 12:14:39 -0400

    Update the database context class - SchoolContext.cs

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Data
@@ -14,6 +10,15 @@ namespace ContosoUniversity.Data
         {
         }
 
-        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
